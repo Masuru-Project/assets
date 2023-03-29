db61884a1f07966454d037fdfe8ddc7b1939f9ca
Masuru(Aomsin) <skinmaster133@gmail.com>
2023-03-30 05:59:21 +0700

    ratelimit assets

---
+++ ratelimit/js/masuru.js
@@ -0,0 +1,14 @@
+var styles = [
+    'background-color: #ff3ff9'
+    , 'border: 1px solid #ff3ff9'
+    , 'color: white'
+    , 'display: block'
+    , 'text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3)'
+    , 'box-shadow: 0 1px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset'
+    , 'line-height: 40px'
+    , 'text-align: center'
+    , 'font-weight: bold'
+].join(';');
+setTimeout(() => {
+    console.log('%c Masuru-Nginx ', styles)
+}, 2000)
